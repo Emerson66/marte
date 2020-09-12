29e4d9b10b66339e08113b6cd559c5b62d68885d
Emerson Reis <emersonsinho66@gmail.com>
2020-09-12 17:14:23 -0400

    Projeto criado

---
+++ src/main/java/com/nosreme/marte/MarteApplication.java
@@ -0,0 +1,13 @@
+package com.nosreme.marte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nosreme/marte/MarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosreme.marte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
