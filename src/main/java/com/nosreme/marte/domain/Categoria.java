943ea018c14a372dfb3b626d91d64b3a9e1d36d5
Emerson Reis <emersonsinho66@gmail.com>
2020-09-13 00:04:10 -0400

    Testando a primeira class de dominio - Categoria

---
+++ src/main/java/com/nosreme/marte/domain/Categoria.java
@@ -0,0 +1,71 @@
+package com.nosreme.marte.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String Nome;
+	
+	public Categoria() {
+		
+	}
+
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		Nome = nome;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return Nome;
+	}
+
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
