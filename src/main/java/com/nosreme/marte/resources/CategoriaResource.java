690a9348b88e0ec6aa949f5ad64d548c913e76d5
Emerson Reis <emersonsinho66@gmail.com>
2020-09-12 23:40:05 -0400

    REST testado

---
+++ src/main/java/com/nosreme/marte/resources/CategoriaResource.java
@@ -0,0 +1,14 @@
+package com.nosreme.marte.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando.";
+	}
+}
