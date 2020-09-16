fd095c53f0c313310ea6d942f2933cf5cb114237
Emerson Reis <emersonsinho66@gmail.com>
2020-09-16 15:51:57 -0400

    Implementação das entidades Cidade e Estado

---
+++ src/main/java/com/nosreme/marte/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.nosreme.marte.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nosreme.marte.domain.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/nosreme/marte/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.nosreme.marte.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nosreme.marte.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
